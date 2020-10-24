a0fa01477a761f58d916a08d6f03f17be4b5dad2
mihai <iampenguinsource@gmail.com>
2020-10-24 09:02:59 -0400

    get lat lon api

--- index.js
+++ index.js
@@ -7,6 +7,26 @@ const port = 3000
 app.use(express.static('./react/build'))
 
 
+const fetch = require('node-fetch')
+
+const SENDINBLUE_API_KEY = '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'
+const GOOGLE_API_KEY = 'AIzaSyB1lsjXlikz-q7pMNliSGsoVE7zXB_wTcY'
+
+
+// https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,
+// +Mountain+View,+CA&key=YOUR_API_KEY
+
+const run = async () => {
+  const response = await fetch('https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key='+GOOGLE_API_KEY)
+  const resp = await response.json()
+  console.log('resp is', resp)
+
+  console.log('...', resp.results[0].geometry)
+}
+
+// run()
+
+
 console.log('whats up...')
 
 app.get('/api/sup', (req, res) => {
