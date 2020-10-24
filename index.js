a8b93abf5ac8aa0c1ef84a5b238afad1678bc421
mihai <iampenguinsource@gmail.com>
2020-10-24 08:38:10 -0400

    init

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require('express')
 const app = express()
+
+
 const port = 3000
 
 app.use(express.static('./react/build'))
+
+
+console.log('whats up...')
 
 app.get('/api/sup', (req, res) => {
   res.send('Hello World 2!')
