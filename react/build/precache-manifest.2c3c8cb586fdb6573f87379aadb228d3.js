f7640c06db24fd44a3082a09f6bd77b45e418608
mihai <iampenguinsource@gmail.com>
2020-10-24 08:29:21 -0400

    setting up env

---
+++ react/build/precache-manifest.2c3c8cb586fdb6573f87379aadb228d3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e9ea6697a614d562da23decba070311",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fc86944178a875ecd118",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f8373a438c6aa6054957",
+    "url": "/static/js/2.14e6cfe8.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/static/js/2.14e6cfe8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc86944178a875ecd118",
+    "url": "/static/js/main.fd6d90da.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
